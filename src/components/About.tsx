80af82f875a920caa25631a224a864063a2e0bab
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-01-26 22:48:11 -0300

    adding some details

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,11 +17,6 @@ type Props = {}
 export default function About({}: Props) {
   return (
     <section id="about" className='min-h-screen max-w-5xl mx-auto py-[5rem] flex flex-col items-center md:flex-row md:justify-center gap-[3rem] relative'>
-      <div className='w-[300px] md:w-[500px]'>
-        <h3 className='font-jetBrains text-xl md:text-2xl mb-5'>about me.</h3>
-        <p className='mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita recusandae praesentium sed, dolorum impedit, veniam, nostrum aut ipsum debitis sint vero ea dicta voluptatum. Ab nesciunt quasi corrupti quisquam quaerat!</p>
-        <p className='mb-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita recusandae praesentium sed, dolorum impedit, veniam, nostrum aut ipsum debitis sint vero ea dicta voluptatum. Ab nesciunt quasi corrupti quisquam quaerat!</p>
-      </div>
       <motion.div 
         initial={{ x: 50, opacity: 0 }} 
         whileInView={{ x:0, opacity: 1}} 
@@ -29,6 +24,11 @@ export default function About({}: Props) {
         className='rounded-full w-[250px] h-[250px] md:w-[350px] md:h-[350px] overflow-hidden'>
         <Image src={aboutImage} alt='Luca Pandolfelli' width={350} height={350} />
       </motion.div>
+      <div className='w-[300px] md:w-[500px]'>
+        <h3 className='font-jetBrains text-xl md:text-2xl mb-5'>about me.</h3>
+        <p className='mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita recusandae praesentium sed, dolorum impedit, veniam, nostrum aut ipsum debitis sint vero ea dicta voluptatum. Ab nesciunt quasi corrupti quisquam quaerat!</p>
+        <p className='mb-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita recusandae praesentium sed, dolorum impedit, veniam, nostrum aut ipsum debitis sint vero ea dicta voluptatum. Ab nesciunt quasi corrupti quisquam quaerat!</p>
+      </div>
     </section>
   )
 }
