5f17f4451a3c1e69b1ec8679060a290e1d3a519e
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-01-27 12:09:27 -0300

    using react-use-downloader to create a downloader button from homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
+
+import useDownloader from 'react-use-downloader'
 import { motion } from 'framer-motion'
 
 type Props = {}
 
 export default function Home({}: Props) {
+  const { download } = useDownloader()
+  const fileUrl = '/files/Pandolfelli Gian Luca CV.pdf'
+  const fileName = 'Pandolfelli Gian Luca CV.pdf'
+
   return (
     <section className='sm:px-8 flex items-center justify-center flex-col relative h-screen' id='home'>
       <div className='text-center'>
@@ -19,8 +25,8 @@ export default function Home({}: Props) {
           transition={{ duration: 1.5 }}
           className='font-jetBrains text-sm tracking-tighter font-thin mb-8 md:text-xl'>FullStack Javascript Developer. Computer Technician. Informatics Lover.</motion.h3>
         <div className='flex justify-center gap-[.75rem] text-sm md:text-base'>
-          <Link className='home_button w-[125px] md:w-[145px]' href='mailto:lucapandolfelli2002@gmail.com'>Contact Me</Link>
-          <Link className='home_button w-[125px] md:w-[145px]' href='#'>Download CV</Link>
+          <Link className='home_button w-[125px] md:w-[145px]' href='mailto:lucapandolfelli2002@gmail.com' target={'_blank'} rel='noreferrer' aria-label='mail'>Contact Me</Link>
+          <button onClick={() => download(fileUrl, fileName)} className='home_button w-[125px] md:w-[145px]'>Download CV</button>
         </div>
       </div>
       <a href="#about" className='animate-bounce p-4 border rounded-full w-[40px] h-[40px] flex items-center justify-center absolute bottom-4'><i className="fa-solid fa-arrow-down"></i></a>
