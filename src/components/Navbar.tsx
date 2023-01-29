7a089f84c2bb980728220548a976936cec7ff665
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-01-29 11:18:13 -0300

    adding navbar funcionality in responsive design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar({}: Props) {
       window.scrollY >= 100 ? setShowNavbar('navbar show') : setShowNavbar('navbar')
     }
     window.addEventListener('scroll', changeNavbarVisibility)
-    
+
     if (screen.width > 640) {
       setToggleNavbar(true)
     }
@@ -23,16 +23,19 @@ export default function Navbar({}: Props) {
         ? <button onClick={() => setToggleNavbar(false)} className='text-2xl absolute top-[1rem] right-[2rem] sm:hidden'><i className="fa-solid fa-x"></i></button>
         : <button onClick={() => setToggleNavbar(true)} className='text-2xl absolute top-[1rem] right-[2rem] sm:hidden'><i className="fa-solid fa-bars"></i></button>
       }
-      { toggleNavbar && <nav className='bg-[#1d1f25] h-screen w-full sm:h-auto sm:visible flex justify-center items-center'>
-        <ul className='font-jetBrains list-none flex flex-col items-center gap-[2rem] sm:flex-row sm:gap-3 text-lg font-thin lowercase'>
-          <li><a href="#home" className='transition-all duration-300 hover:text-amber-600'>Inicio</a><span className='hidden sm:inline pl-1'>,</span></li>
-          <li><a href="#about" className='transition-all duration-300 hover:text-amber-600'>Sobre Mí</a><span className='hidden sm:inline pl-1'>,</span></li>
-          <li><a href="#projects" className='transition-all duration-300 hover:text-amber-600'>Proyectos</a><span className='hidden sm:inline pl-1'>,</span></li>
-          <li><a href="#experience" className='transition-all duration-300 hover:text-amber-600'>Experiencia</a><span className='hidden sm:inline pl-1'>,</span></li>
-          <li><a href="#technologies" className='transition-all duration-300 hover:text-amber-600'>Tecnologías</a><span className='hidden sm:inline pl-1'>,</span></li>
-          <li><a href="#contact" className='transition-all duration-300 hover:text-amber-600'>Contacto</a></li>
-        </ul>
-      </nav>}
+      {
+        toggleNavbar && 
+        <nav className='bg-[#1d1f25] h-screen w-full transition-all duration-500 ease-in sm:h-auto sm:visible flex justify-center items-center'>
+          <ul className='font-jetBrains list-none flex flex-col items-center gap-[2rem] sm:flex-row sm:gap-3 text-lg font-thin lowercase'>
+            <li><a href="#home" className='transition-all duration-300 hover:text-amber-600'>Inicio</a><span className='hidden sm:inline pl-1'>,</span></li>
+            <li><a href="#about" className='transition-all duration-300 hover:text-amber-600'>Sobre Mí</a><span className='hidden sm:inline pl-1'>,</span></li>
+            <li><a href="#projects" className='transition-all duration-300 hover:text-amber-600'>Proyectos</a><span className='hidden sm:inline pl-1'>,</span></li>
+            <li><a href="#experience" className='transition-all duration-300 hover:text-amber-600'>Experiencia</a><span className='hidden sm:inline pl-1'>,</span></li>
+            <li><a href="#technologies" className='transition-all duration-300 hover:text-amber-600'>Tecnologías</a><span className='hidden sm:inline pl-1'>,</span></li>
+            <li><a href="#contact" className='transition-all duration-300 hover:text-amber-600'>Contacto</a></li>
+          </ul>
+        </nav>
+      }
     </header>
   )
 }
