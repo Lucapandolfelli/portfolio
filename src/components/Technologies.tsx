ec3d2e6e7d8f14335c3f321103bce2be0afcc777
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-01-29 11:25:14 -0300

    adding animations on technlogies section

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import { motion } from 'framer-motion'
+
 import TechnologyCard from './TechnologyCard'
 
 type Props = {}
@@ -24,17 +27,31 @@ const technologies = [
   { id: 18, name: 'WordPress', image: 'wordpress.png'},
 ]
 
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.05
+    }
+  }
+}
+
 export default function Technologies({}: Props) {
   return (
     <section id="technologies" className='min-h-screen max-w-5xl mx-auto py-[5rem] flex flex-col items-center justify-center gap-[3rem] md:gap-[4rem]'>
       <div className="text-center">
         <h3 className='font-jetBrains text-xl md:text-2xl'>tecnologías.</h3>
       </div>
-      <div className='flex flex-row justify-center flex-wrap gap-[1.25rem] xl:gap-[2rem]'>
+      <motion.ul 
+        variants={container}
+        initial='hidden'
+        whileInView='show'
+        className='flex flex-row justify-center flex-wrap gap-[1.25rem] xl:gap-[2rem]'>
         {technologies.map((technology) => (
           <TechnologyCard technology={technology} key={technology.id} />
         ))}
-      </div>
+      </motion.ul>
     </section>
   )
 }
--- src/components/TechnologyCard.tsx
+++ src/components/TechnologyCard.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
+
+import { motion } from 'framer-motion'
+
 import { Skill } from '../types'
 
 type Props = {
@@ -7,16 +10,29 @@ type Props = {
   key: number
 }
 
+const itemAnimate = {
+  hidden: {
+    opacity: 0,
+    scale: 0
+  },
+  show: {
+    opacity: 1,
+    scale: 1
+  }
+}
+
 export default function TechnologyCard({ technology }: Props) {
   const { name, image} = technology
   return (
-    <article className='flex flex-col items-center w-[90px] md:w-[120px] gap-2'>
+    <motion.article 
+      variants={itemAnimate}
+      className='flex flex-col items-center w-[90px] md:w-[120px] gap-2'>
       <div className="border rounded-full h-[90px] w-[90px] md:h-[120px] md:w-[120px] border-slate-400 overflow-hidden">
         <Image src={`/images/technologies/${image}`} alt={name} width={120} height={120} />
       </div>
       <div className='text-[#d9d9d9]/75'>
         <p>{name}</p>
       </div>
-    </article>
+    </motion.article>
   )
 }
