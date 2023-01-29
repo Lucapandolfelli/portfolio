3343dcce8bb7ab669ca7acf5cf7bb7c014a0fe10
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-01-29 11:09:16 -0300

    adding navbar funcionality in responsive design

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,15 +18,15 @@ export default function Home({}: Props) {
           initial={{ x: 50, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: .5 }}
-          className='font-bold text-[4rem] text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 leading-none mb-6 lg:text-[9rem]'>Luca Pandolfelli</motion.h1>
+          className='font-bold text-[4.25rem] text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 leading-none mb-6 lg:text-[9rem]'>Luca Pandolfelli</motion.h1>
         <motion.h3 
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1.5 }}
           className='font-jetBrains text-sm tracking-tighter font-thin mb-8 md:text-xl'>FullStack Javascript Developer. Técnico en Informática. Apasionado por la tecnología.</motion.h3>
         <div className='flex justify-center gap-[.75rem] text-sm md:text-base'>
-          <Link className='home_button w-[125px] md:w-[145px]' href='mailto:lucapandolfelli2002@gmail.com' target={'_blank'} rel='noreferrer' aria-label='mail'>Contáctame</Link>
-          <button onClick={() => download(fileUrl, fileName)} className='home_button w-[125px] md:w-[145px]'>Descargar CV</button>
+          <Link className='home_button w-[130px] md:w-[145px]' href='mailto:lucapandolfelli2002@gmail.com' target={'_blank'} rel='noreferrer' aria-label='mail'>Contáctame</Link>
+          <button onClick={() => download(fileUrl, fileName)} className='home_button w-[130px] md:w-[145px]'>Descargar CV</button>
         </div>
       </div>
       <a href="#about" className='animate-bounce p-4 border rounded-full w-[40px] h-[40px] flex items-center justify-center absolute bottom-4'><i className="fa-solid fa-arrow-down"></i></a>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,26 +4,35 @@ type Props = {}
 
 export default function Navbar({}: Props) {
   const [showNavbar, setShowNavbar] = useState('navbar')
+  const [toggleNavbar, setToggleNavbar] = useState(false)
 
   useEffect(() => {
     const changeNavbarVisibility = () => {
-      window.scrollY >= 100 ? setShowNavbar('navbar active') : setShowNavbar('navbar')
+      window.scrollY >= 100 ? setShowNavbar('navbar show') : setShowNavbar('navbar')
     }
     window.addEventListener('scroll', changeNavbarVisibility)
+    
+    if (screen.width > 640) {
+      setToggleNavbar(true)
+    }
   }, [])
 
   return (
     <header className={showNavbar}>
-      <nav className='flex justify-center items-center'>
-        [<ul className='list-none flex gap-3 text-xl font-thin lowercase px-2'>
-          <li><a href="#home" className='transition-all duration-300 hover:text-amber-600'>Inicio</a><span className='pl-1'>,</span></li>
-          <li><a href="#about" className='transition-all duration-300 hover:text-amber-600'>Sobre Mí</a><span className='pl-1'>,</span></li>
-          <li><a href="#projects" className='transition-all duration-300 hover:text-amber-600'>Proyectos</a><span className='pl-1'>,</span></li>
-          <li><a href="#experience" className='transition-all duration-300 hover:text-amber-600'>Experiencia</a><span className='pl-1'>,</span></li>
-          <li><a href="#technologies" className='transition-all duration-300 hover:text-amber-600'>Tecnologías</a><span className='pl-1'>,</span></li>
+      { toggleNavbar 
+        ? <button onClick={() => setToggleNavbar(false)} className='text-2xl absolute top-[1rem] right-[2rem] sm:hidden'><i className="fa-solid fa-x"></i></button>
+        : <button onClick={() => setToggleNavbar(true)} className='text-2xl absolute top-[1rem] right-[2rem] sm:hidden'><i className="fa-solid fa-bars"></i></button>
+      }
+      { toggleNavbar && <nav className='bg-[#1d1f25] h-screen w-full sm:h-auto sm:visible flex justify-center items-center'>
+        <ul className='font-jetBrains list-none flex flex-col items-center gap-[2rem] sm:flex-row sm:gap-3 text-lg font-thin lowercase'>
+          <li><a href="#home" className='transition-all duration-300 hover:text-amber-600'>Inicio</a><span className='hidden sm:inline pl-1'>,</span></li>
+          <li><a href="#about" className='transition-all duration-300 hover:text-amber-600'>Sobre Mí</a><span className='hidden sm:inline pl-1'>,</span></li>
+          <li><a href="#projects" className='transition-all duration-300 hover:text-amber-600'>Proyectos</a><span className='hidden sm:inline pl-1'>,</span></li>
+          <li><a href="#experience" className='transition-all duration-300 hover:text-amber-600'>Experiencia</a><span className='hidden sm:inline pl-1'>,</span></li>
+          <li><a href="#technologies" className='transition-all duration-300 hover:text-amber-600'>Tecnologías</a><span className='hidden sm:inline pl-1'>,</span></li>
           <li><a href="#contact" className='transition-all duration-300 hover:text-amber-600'>Contacto</a></li>
-        </ul>]
-      </nav>
+        </ul>
+      </nav>}
     </header>
   )
 }
