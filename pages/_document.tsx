0ea911f8806748fed70b3c8dacf22f5ec75d887a
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-02-03 11:08:20 -0300

    adding open graph to the project

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,8 +11,8 @@ export default function Document() {
         <meta property="og:url" content="https://lucapandolfelli.com"/>
         <meta property="og:image" content="https://github.com/Lucapandolfelli/portfolio/blob/main/public/images/screenshot.png"/>
         <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="400" />
-        <meta property="og:image:height" content="300" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="Luca Pandolfelli Portfolio Website" />
         <meta property="og:description" content="Portfolio de Luca Pandolfelli. Fullstack Javascript Developer. Técnico en Informática. Apasionado por la tecnología."/>
         <meta property="og:type" content="website"/>
