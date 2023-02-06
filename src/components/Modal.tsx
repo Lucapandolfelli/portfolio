885c48223fb03219376df5a851ac9a2dcc8473e6
lucapandolfelli <lucapandolfelli2002@gmai.com>
2023-02-06 16:56:41 -0300

    fixing projects card buttons on mobile version

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -19,7 +19,7 @@ export default function Modal({ showModal, project }: Props) {
         exit={{ opacity: 0 }}
         transition={{ duration: .25 }}
         className="bg-[#282B33] rounded-md w-[310px] lg:w-[500px] xl:w-[700px] h-fit relative shadow-xl z-50">
-        <button className="hidden absolute top-0 left-0 right-0 bottom-[-120%] m-auto sm:block sm:bottom-[unset] sm:m-0 sm:left-[unset] sm:top-[-2rem] sm:right-[-2rem] transition-all duration-300 ease-in-out hover:scale-[1.05]" onClick={() => showModal(false)}><i className="fa-solid fa-xmark text-2xl"></i></button>
+        <button className="absolute top-0 left-[calc(50%_-_7.5px)] bottom-[-120%] sm:block sm:bottom-[unset] sm:m-0 sm:left-[unset] sm:top-[-2rem] sm:right-[-2rem] transition-all duration-300 ease-in-out hover:scale-[1.05]" onClick={() => showModal(false)}><i className="fa-solid fa-xmark text-2xl"></i></button>
         <div className="hidden xl:block w-full">
           <Image src={`/images/projects/${ project.image }`} alt={ project.title } width={700} height={200} className='rounded-t-md' />
         </div>
